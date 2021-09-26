b8f450239a9552a376dbe5bae3d74300a93c6624
Saloni Potdar <salonispotdar@gmail.com>
2021-09-26 20:29:48 +0530

    cleared messege in Add.java

--- Add.java
+++ Add.java
@@ -6,7 +6,7 @@ public class Add {
            int num1 = 5, num2 = 15, sum;
            sum = num1 + num2;
      
-           System.out.println("Sum of these numbers: "+sum);
+           System.out.println("Sum  "+sum);
         }
      
     
